99f7912eed07f04b7123a7b2d7615e501ec9c9b1
DrugPerikla <87117005+DrugPerikla@users.noreply.github.com>
2021-07-09 05:18:08 +0300

    Update 06_hw.py

--- Module3/home_work/06_hw.py
+++ Module3/home_work/06_hw.py
@@ -35,11 +35,35 @@ items = [
 print("Товары на складе представлены брэндами: ")
 
 # TODO: your code here
+all_brands=[]
+for i in items:
+    all_brands.append(i.get("brand"))
+list_brand = set(all_brands)
+print(list_brand)
 
 print("На складе больше всего товаров брэнда(ов): ")
 
+
 # TODO: your code here
+count_items = {x: all_brands.count(x) for x in list_brand}
+#print(count_items.items())
+
+max_count = 0
+for brand, summa in count_items.items():
+    if summa >= max_count:
+        max_count = summa
+for brand, summa in count_items.items():
+    if summa == max_count:
+        print(brand)
+
 
 print("На складе самый дорогой товар брэнда(ов): ")
 
 # TODO: your code here
+max_price = 0
+for rich in items:
+    if rich.get("price") >= max_price:
+        max_price = rich.get("price")
+for rich in items:
+    if rich.get("price") == max_price:
+        print(f"{rich.get("brand")}")
